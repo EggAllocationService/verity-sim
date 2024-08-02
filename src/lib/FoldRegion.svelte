b91c4db99456156a5d65b9d2813d87bed44c0669
Kyle Smith <kyleafmine@gmail.com>
2024-08-02 12:50:37 -0300

    Fix overflow and format

--- src/lib/FoldRegion.svelte
+++ src/lib/FoldRegion.svelte
@@ -6,30 +6,39 @@
   function toggle() {
     expanded = !expanded;
     if (expanded) {
-      icon.animate([
-        { transform: "rotateZ(90deg)" },
-        { transform: "rotateZ(0deg)" }
-      ], { duration: speed, fill: "forwards" });
+      icon.animate(
+        [{ transform: "rotateZ(90deg)" }, { transform: "rotateZ(0deg)" }],
+        { duration: speed, fill: "forwards" },
+      );
     } else {
-      icon.animate([
-        { transform: "rotateZ(0deg)" },
-        { transform: "rotateZ(90deg)" }
-      ], { duration: speed, fill: "forwards" });
+      icon.animate(
+        [{ transform: "rotateZ(0deg)" }, { transform: "rotateZ(90deg)" }],
+        { duration: speed, fill: "forwards" },
+      );
     }
 
     // calculate height of the content
     let height = content.clientHeight;
     // animate region
     if (expanded) {
-      region.animate([
-      { height: `${btn.clientHeight}px` },
-      { height: `${btn.clientHeight + height + 12}px` }
-    ], { duration: speed, fill: "forwards" });
+      region.animate(
+        [
+          { height: `${btn.clientHeight}px` },
+          { height: `${btn.clientHeight + height + 12}px` },
+        ],
+        { duration: speed, fill: "forwards" },
+      ).onfinish = () => {
+        region.style.overflow = "visible";
+      };
     } else {
-      region.animate([
-       { height: `${btn.clientHeight + height + 12}px` },
-       { height: `${btn.clientHeight}px` }
-    ], { duration: speed, fill: "forwards" });
+      region.style.overflow = "hidden";
+      region.animate(
+        [
+          { height: `${btn.clientHeight + height + 12}px` },
+          { height: `${btn.clientHeight}px` },
+        ],
+        { duration: speed, fill: "forwards" },
+      );
     }
   }
   let btn: HTMLElement;
@@ -41,11 +50,9 @@
       region.style.height = `${btn?.clientHeight}px`;
     }
   }
-
-
 </script>
 
-<div class="fold-region" class:expanded={expanded} bind:this={region}>
+<div class="fold-region" class:expanded bind:this={region}>
   <button class="header" on:click={toggle} bind:this={btn}>
     <span>{title}</span>
     <span class="icon" bind:this={icon}>▼</span>
@@ -58,7 +65,7 @@
 <style>
   button {
     width: 100%;
-    padding: 0.5em;
+    padding: 0.8em;
     background: transparent !important;
     color: white;
     border: none;
